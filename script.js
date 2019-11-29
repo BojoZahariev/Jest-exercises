beb6636902efccb9c9363e6c956f28672d3d8e5c
Bojo <bozhidar_zahariev@hotmail.com>
2019-11-29 12:07:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,35 @@ return string.split('').reverse().join('');
 test('hello to become olleh', () => {
   expect(reverseString('hello')).toBe('olleh');
 });
+
+
+//A calculator object that contains the basic operations: add, subtract, divide, and multiply.
+
+var calculator = {
+  first: 3,
+  second : 2,
+  
+  add : function() {
+    return this.first + this.second;
+  },
+
+  subtract : function() {
+    return this.first - this.second;
+  },
+
+  divide : function() {
+    return this.first / this.second;
+  },
+
+  multiply : function() {
+    return this.first * this.second;
+  },
+
+};
+
+test('calculator', () => {
+  expect(calculator.add()).toEqual(5);
+  expect(calculator.subtract()).toEqual(1);
+  expect(calculator.divide()).toBeCloseTo(1.5);
+  expect(calculator.multiply()).toEqual(6);
+});
